976d652e40cda2c119905cc74e942f325845bf53
Attilio <parrotchaffinch@gmail.com>
2018-09-30 11:58:30 -0400

    Add jest, conflicting with react-scripts

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import renderer from 'react-test-renderer';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+test('Link changes the class when hovered', () => {
+  const component = renderer.create(
+    <App />,
+  );
+  let tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
 });
+
